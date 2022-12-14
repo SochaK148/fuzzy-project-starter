0fb931f57ee305e7f002434be597682557639661
SochaK148 <konrad.socha10@wp.pl>
2022-12-14 16:44:54 +0000

    added a test fuzzy matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,87 @@
 #include <storm/api/storm.h>
-#include <storm-parsers/api/storm-parsers.h>
-#include <storm-parsers/parser/PrismParser.h>
-#include <storm/storage/prism/Program.h>
-#include <storm/storage/jani/Property.h>
-#include <storm/modelchecker/results/CheckResult.h>
-#include <storm/modelchecker/results/ExplicitQuantitativeCheckResult.h>
-
 #include <storm/utility/initialize.h>
+#include <storm/models/sparse/Dtmc.h>
+#include <storm/storage/TriangularFuzzyNumber.h>
+#include <storm/storage/FuzzyAnalysisResult.h>
+#include <vector>
 
-typedef storm::models::sparse::Dtmc<double> Dtmc;
+typedef storm::models::sparse::Dtmc<storm::storage::TriangularFuzzyNumber> Dtmc;
 typedef storm::modelchecker::SparseDtmcPrctlModelChecker<Dtmc> DtmcModelChecker;
 
-bool check(std::string const& path_to_model, std::string const& property_string) {
-    // Assumes that the model is in the prism program language format and parses the program.
-    auto program = storm::parser::PrismParser::parse(path_to_model);
-    // Code snippet assumes a Dtmc
-    assert(program.getModelType() == storm::prism::Program::ModelType::DTMC);
-    // Then parse the properties, passing the program to give context to some potential variables.
-    auto properties = storm::api::parsePropertiesForPrismProgram(property_string, program);
-    // Translate properties into the more low-level formulae.
-    auto formulae = storm::api::extractFormulasFromProperties(properties);
+template<typename ValueType>
+storm::storage::SparseMatrix<ValueType> createMatrix() {
+    storm::storage::SparseMatrixBuilder<ValueType> matrixBuilder;
+    ValueType prob = storm::storage::TriangularFuzzyNumber(0.4,0.5,0.6);
+    matrixBuilder.addNextValue(0, 1, prob);
+    matrixBuilder.addNextValue(0, 2, storm::storage::TriangularFuzzyNumber(0.4,0.5,0.6));
+    matrixBuilder.addNextValue(1, 0, storm::storage::TriangularFuzzyNumber(0.1,0.2,0.3));
+    matrixBuilder.addNextValue(1, 3, storm::storage::TriangularFuzzyNumber(0.7,0.8,0.9));
+    matrixBuilder.addNextValue(2, 2, storm::storage::TriangularFuzzyNumber(0.1,0.2,0.3));
+    matrixBuilder.addNextValue(2, 4, storm::storage::TriangularFuzzyNumber(0.7,0.8,0.9));
+    matrixBuilder.addNextValue(3, 3, storm::storage::TriangularFuzzyNumber(0,1.0,0));
+    matrixBuilder.addNextValue(4, 4, storm::storage::TriangularFuzzyNumber(0,1.0,0));
+
+    return matrixBuilder.build();
+}
+
+std::vector<std::vector<double>> createMatrix2() {
+    std::vector<std::vector<double>> v;
+    v.push_back({0, 0.5, 0.5, 0, 0});
+    v.push_back({0.2, 0, 0, 0.8, 0});
+    v.push_back({0, 0, 0.2, 0, 0.8});
+    v.push_back({0, 0, 0, 1, 0});
+    v.push_back({0, 0, 0, 0, 1});
+
+    return v;
+}
+
+template<typename ValueType, typename RewardModelType>
+std::shared_ptr<storm::models::sparse::Model<ValueType, RewardModelType>> createModel(storm::storage::SparseMatrix<ValueType> matrix, size_t goalState) {
+    // Initialize
+    size_t stateSize = matrix.getColumnCount();
+    auto modelComponents = std::make_shared<storm::storage::sparse::ModelComponents<ValueType, RewardModelType>>();
+    modelComponents->stateLabeling = storm::models::sparse::StateLabeling(stateSize);
+
+    // Set labels
+    // First state is initial state
+    modelComponents->stateLabeling.addLabel("init");
+    modelComponents->stateLabeling.addLabelToState("init", 0);
+    // Set goal state
+    modelComponents->stateLabeling.addLabel("goal");
+    modelComponents->stateLabeling.addLabelToState("goal", goalState);
+
+    // Build transition matrix
+    modelComponents->transitionMatrix = matrix;
+
+    // Build model
+    return storm::utility::builder::buildModelFromComponents(storm::models::ModelType::Dtmc, std::move(*modelComponents));
+}
+
+void check() {
+    // Create Matrix
+    auto matrix = createMatrix<storm::storage::TriangularFuzzyNumber>();
+    auto matrix2 = createMatrix2();
     
-    // Now translate the prism program into a DTMC in the sparse format.
-    // Use the formulae to add the correct labelling.
-    auto model = storm::api::buildSparseModel<double>(program, formulae)->template as<Dtmc>();
+    std::cout << matrix << std::endl;
+    storm::storage::FuzzyAnalysisResult test(matrix);
+    std:: cout << "Alpha cut: " << test.getAlphaCut(storm::storage::TriangularFuzzyNumber(0.4,0.5,0.6), 0.5) << std::endl;
+    test.isFeasible(matrix2, 0.5);
     
-    // Create a model checker on top of the sparse engine.
-    auto checker = std::make_shared<DtmcModelChecker>(*model);
-    // Create a check task with the formula. Run this task with the model checker.
-    auto result = checker->check(storm::modelchecker::CheckTask<>(*(formulae[0]), true));
-    assert(result->isExplicitQuantitativeCheckResult());
-    // Use that we know that the model checker produces an explicit quantitative result
-    auto quantRes = result->asExplicitQuantitativeCheckResult<double>();
-    // Now compare the result at the first initial state of the model with 0.5.
-    return quantRes[*model->getInitialStates().begin()] > 0.5;
+    // Set goal state
+    size_t goalState = 4;
+    // Create model
+    auto model = createModel<storm::storage::TriangularFuzzyNumber, storm::models::sparse::StandardRewardModel<storm::storage::TriangularFuzzyNumber>>(matrix, goalState);
+
+    // Print information about model
+    model->printModelInformationToStream(std::cout);
+    matrix = model->getTransitionMatrix();
+    std::cout << matrix << std::endl;
 }
 
 
 int main (int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cout << "Needs exactly 2 arguments: model file and property" << std::endl;
-        return 1;
+    if (argc > 1) {
+        std::cout << "Arguments are ignored" << std::endl;
     }
 
     // Init loggers
@@ -49,7 +90,5 @@ int main (int argc, char *argv[]) {
     storm::settings::initializeAll("storm-starter-project", "storm-starter-project");
 
     // Call function
-    auto result = check(argv[1], argv[2]);
-    // And print result
-    std::cout << "Result > 0.5? " << (result ? "yes" : "no") << std::endl;
+    check();
 }
